9a237a5e7567899945e2273c3c641a241144e2cf
Cole Krumbholz <cole.krumbholz@gmail.com>
2012-04-29 03:18:22 -0700

    finishing touches to basic template

--- basic/app/scripts/main.js
+++ basic/app/scripts/main.js
@@ -1,18 +1,60 @@
-var MainView = Backbone.View.extend({
-    render: function() 
-    {
-        var content = window.JST.main({app_id: window.backlift_app_id});
-        $('body').html(content);
+// Copyright (c) 2012 SendSpree Inc.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining 
+// a copy of this software and associated documentation files (the 
+// "Software"), to deal in the Software without restriction, including 
+// without limitation the rights to use, copy, modify, merge, publish, 
+// distribute, sublicense, and/or sell copies of the Software, and to 
+// permit persons to whom the Software is furnished to do so, subject 
+// to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be 
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-        // UNCOMMENT ME TO ENABLE DINOSAUR TRACKS!
+var MainView = Backbone.View.extend({
+    render: function() {
+        
+        // Render the main.jst template.
+        var content = window.JST.main({
+            app_id: window.backlift_app_id
+        }); 
+
+        // add the content to the DOM
+        this.$el.html(content);
+
+        // // UNCOMMENT ME TO ENABLE DINOSAUR TRACKS!
         // var tracksview = new window.TracksView();
-        // $('#tracks-container').html(tracksview.render().el);
+        // this.$('#tracks-container').html(tracksview.render().el);
 
         return this;
     },
 });
 
-$(function(){
-    var mv = new MainView();
-    mv.render();    
+// MainRouter: This simple example only has one page. More
+// complex sites will include additional routes for each
+// page on the website.
+var MainRouter = Backbone.Router.extend({
+    routes: {
+        "": "handleHome",
+    },
+
+    handleHome: function() {
+        // render the main view into the body of the page
+        var mainView = new MainView();
+        $('body').empty();
+        $('body').append(mainView.render().el);
+    },
+});
+
+$(function() {
+    var App = new MainRouter();
+    Backbone.history.start(); 
 });
--- basic/app/scripts/tracks.js
+++ basic/app/scripts/tracks.js
@@ -1,44 +1,78 @@
+// Copyright (c) 2012 SendSpree Inc.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining 
+// a copy of this software and associated documentation files (the 
+// "Software"), to deal in the Software without restriction, including 
+// without limitation the rights to use, copy, modify, merge, publish, 
+// distribute, sublicense, and/or sell copies of the Software, and to 
+// permit persons to whom the Software is furnished to do so, subject 
+// to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be 
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 var Tracks = Backbone.Collection.extend({
-	model: Backbone.Model,
-	url: 'backlift/tracks',
+    url: 'backlift/tracks',
 });
 
 window.TracksView = Backbone.View.extend({
 
-	className: 'tracksview',
+    className: 'tracksview',
 
-	initialize: function() {
+    initialize: function() {
+
+        // instantiate a new Tracks collection and
+        // initialize it with data from the server
         this.collection = new Tracks();
         this.collection.fetch();
 
-		this.collection.on('add', this.render, this);
-		this.collection.on('reset', this.render, this);
-		this.lastFoot = 'right';
-	},
+        // call this.render whenever the collection
+        // changes
+        this.collection.on('add', this.render, this);
+        this.collection.on('reset', this.render, this);
 
-	events: {
-		'click': 'addFootprint',
-	},
+        // start with the left foot
+        this.lastFoot = 'right';
+    },
 
-	addFootprint: function(ev) {
-		var x = ev.pageX - ev.target.offsetLeft - 25;
-		var y = ev.pageY - ev.target.offsetTop - 32;
+    events: {
+        'click': 'addFootprint',
+    },
 
-		var foot = 'left';
-		if (this.lastFoot == 'left') {
-			foot = 'right';
-		}
-		this.lastFoot = foot;
+    addFootprint: function(ev) {
 
-		this.collection.create({xpos: x+'px', ypos: y+'px', foot: foot});
-	},
+        // determin x,y offsets from the upper left
+        // corner of the containing div to the center
+        // of the footprint image.
+        var x = ev.pageX - ev.target.offsetLeft - 25;
+        var y = ev.pageY - ev.target.offsetTop - 32;
 
-	render: function () {
-		var content = window.JST.tracks({
-			collection: this.collection,
-		});
-		$(this.el).html(content);
-		return this;
-	}
+        // select left or right footprint
+        var foot = 'left';
+        if (this.lastFoot == 'left') {
+            foot = 'right';
+        }
+        this.lastFoot = foot;
+
+        this.collection.create({xpos: x+'px', ypos: y+'px', foot: foot});
+    },
+
+    render: function () {
+        // render the tracks using the tracks.jst template
+        var content = window.JST.tracks({
+            collection: this.collection,
+        });
+
+        // add the content to the DOM
+        this.$el.html(content);
+        return this;
+    }
 });
